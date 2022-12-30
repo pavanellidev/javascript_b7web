d166e46160a5ad1284e0f94108543bcd763f074e
andrepavanelli7 <pavanellidev@gmail.com>
2022-12-30 17:29:34 -0300

    estudos 30/12/22

---
+++ 036 - revisao/013.js
@@ -0,0 +1,19 @@
+function subirTela() {
+   window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+   });
+}
+
+function showButton() {
+   if(window.scrollY <= 250) {
+      document.querySelector('.scrollbutton').style.display = 'none';
+   } else {
+      document.querySelector('.scrollbutton').style.display = 'block';
+   }
+}
+
+// setInterval(showButton, 2000);
+
+window.addEventListener('scroll', showButton);
